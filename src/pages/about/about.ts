6a524b117ec85af7db6a30a5026a492e9941432a
Antonella RR <antonella_rr31@hotmail.com>
2017-11-18 12:06:38 -0500

    agregar variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,7 +6,26 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  
+
+nombre: string ="Rossy Ludeña";
+ocupacion: string = "estudiante";
+edad: number =15;
+
+datos:any ={
+ nombre: "Rossy Ludeña",
+ ocupacion: "Estudiante",
+ edad: 15
+
+};
+origen:any ={
+  titulo_origen:"origen",
+  origen:"lima-perú"
+};
+email:any= {
+  titulo: "E-mail",
+  dato:"1dludenarossy@gmail.com"
+}
+
   constructor(public navCtrl: NavController) {
 
   }
